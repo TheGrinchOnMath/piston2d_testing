56a37ad5497b71756144facee31f12fea45f3a76
TheGrinchOnMath <103316040+TheGrinchOnMath@users.noreply.github.com>
2025-03-01 19:19:50 +0100

    working generate_rays, generate_mirrors
    wrote a more skeletal implementation of the render stuff (to make debugging easier)
    generate_Rays works. now to implement intersections

--- src/main.rs
+++ src/main.rs
@@ -1,183 +1,96 @@
-mod io;
 mod physics;
-//mod render;
 
-extern crate piston_window;
-
-use crate::physics::{Mirror, Ray};
-use piston_window::*;
 use std::process::exit;
+use piston::EventLoop;
+use piston_window::*;
 
 fn main() {
+    // configure piston window
     let opengl = OpenGL::V3_2;
-    let mut window: PistonWindow = WindowSettings::new("piston2d", [800;2])
+    let mut window: PistonWindow = WindowSettings::new("piston2d", [800; 2])
         .exit_on_esc(true)
         .graphics_api(opengl)
         .build()
         .unwrap();
     window.set_lazy(true);
-    
-    let mut reflection_count = 0;
-    // init mouse pos by setting it to the middle of the window
-    let mut mouse_pos: [f64; 2] = [window.size().width, window.size().height];
-    // init reset variable. this will be used to trigger the rays be deleted
-    let mut reset = true;
-    // init rays, mirrors vectors. they contain the rays and mirrors.
-    // mirrors are loaded from a json file, rays are generated from cursor position
-    let mut rays: Vec<Ray>;
-    let mirrors: Vec<Mirror> = generate_mirrors(
-        "assets/mirrors.json",
-        [window.size().width, window.size().height],
-    );
-    let ray_count = 11;
-    // this might be stupid, and it probably is, but hey!
-    // big vec of all the generated rays after each reset. might make a lot of memory be used.
-    let mut big_ray_buffer: Vec<Ray> = vec![];
-    // generate very first set of rays
-    rays = physics::generate_rays(physics::Vector2::new_from_array(mouse_pos), ray_count);
-    let mut draw_rays: Vec<Ray> = vec![];
-    for ray in &rays {
-        draw_rays.push(ray.clone());
-    }
 
-    // main event loop
+    // configure "global" variables (cursor pos, etc)
+    let mut mouse_pos = [window.size().width as f64, window.size().height as f64];
+
+    // generate mirrors. these only need to generate once, so are outside of the while loop
+    let mirrors = physics::generate_mirrors(5);
+
+    // main draw loop, call draw() here
     while let Some(e) = window.next() {
-        // add code here to update mouse position
-        if reset || reflection_count == 10 {
-            reflection_count = 0;
-            rays = physics::generate_rays(physics::Vector2::new_from_array(mouse_pos), ray_count);
-            big_ray_buffer.clear();
-            big_ray_buffer = rays.iter().map(|ray| ray.clone()).collect();
-            reset = false;
-        }
-        // dirty implementation again
-        let mut _rays: Vec<Ray> = vec![];
-        for ray in &rays {
-            _rays.push(ray.clone());
-        }
-        let mut _mirrors: Vec<Mirror> = vec![];
-        for mirror in &mirrors {
-            _mirrors.push(mirror.clone());
-        }
-
-        let handler: [Vec<Ray>; 2] =
-            physics::intersection_handler(_rays, _mirrors);
-        
-        reflection_count += 1;
-        rays = handler[0].clone();
-        draw_rays = handler[1].clone();
-        big_ray_buffer.append(&mut draw_rays);
-        
+        // process keyboard events
         if let Some(ref args) = e.press_args() {
             use piston_window::Button::Keyboard;
 
             if *args == Keyboard(Key::Escape) {
-                // kill the process
-                exit(0)
+                exit(0);
             }
-            if *args == Keyboard(Key::Space) {
-                reset = true;
-                // do something here that reloads the keys
-            }
+            // if *args == Keyboard(Key::Space) {
+            //
+            // }
+
         }
+
+        // process mouse events
         if let Some(ref args) = e.mouse_cursor_args() {
-            // update mouse pos
+            // update mouse pos every frame
             mouse_pos = *args;
         }
-        // draw stuff
-        window.draw_2d(&e, |c: Context, g: &mut G2d, _| {
-            let black = [0.0, 0.0, 0.0, 1.0];
-            let blue = [0.0, 0.0, 1.0, 1.0];
-            let white = [1.0;4];
-            clear(black, g);
-            // draw rays
-            for ray in &big_ray_buffer {
-                // println!("{:?}", &ray.distance(ray.vector.clone()));
-                
-                let line_coords = [
-                    ray.start_pos.x,
-                    ray.start_pos.y,
-                    ray.vector.x,
-                    ray.vector.y,
-                ];
-                let thickness = 2.0;
-                line(blue, thickness, line_coords, c.transform, g);
-                rectangle([1.0, 1.0, 0.0, 1.0], [ray.vector.x, ray.vector.y, thickness, 0.0], c.transform, g);
-            }
-            // draw mirrors
-            for mirror in &mirrors {
-                let mirror_coords = [
-                    mirror.start_pos.x,
-                    mirror.start_pos.y,
-                    mirror.end_pos.x,
-                    mirror.end_pos.y,
-                ];
-                let thickness = 3.0;
-                line(white, thickness, mirror_coords, c.transform, g);
-            }
 
-            //line(black, 2.0, [100.0, 200.0, 300.0, 400.0], c.transform, g);
+        // render
+         window.draw_2d(&e, |c:Context, g:&mut G2d, _| {
 
-            /*
 
-            for i in 0..5 {
+             //clear screen
+             let background_color = [1.0; 4];
+             let black = [0.0, 0.0, 0.0, 1.0];
 
-                let c = c.trans(0.0, i as f64 * 100.0);
-                let red = [1.0, 0.0, 0.0, 1.0];
-                let rect = math::margin_rectangle([20.0, 20.0, 60.0, 60.0], i as f64 * 5.0);
-                //rectangle(red, rect, c.transform, g);
-                //Rectangle::new_border(black, 2.0).draw(rect, &c.draw_state, c.transform, g);
-                let green = [0.0, 1.0, 0.0, 1.0];
-                let h = 60.0 * (1.0 - i as f64 / 5.0);
-                let rect = [120.0, 50.0 - h / 2.0, 60.0, h];
-                //ellipse(green, rect, c.transform, g);
-                //Ellipse::new_border(black, 2.0).draw(rect, &c.draw_state, c.transform, g);
-                let blue = [0.0, 0.0, 1.0, 1.0];
-                // circle_arc(blue, 10.0, 0.0, f64::_360() - i as f64 * 1.2, [230.0, 30.0, 40.0, 40.0], c.transform, g);
-                let orange = [1.0, 0.5, 0.0, 1.0];
-                line(orange, 5.0, [320.0 + i as f64 * 15.0, 20.0, 380.0 - i as f64 * 15.0, 80.0],
-                     c.transform, g);
-                let magenta = [1.0, 0.0, 0.5, 1.0];
-                // polygon(magenta, &[[420.0, 20.0], [480.0, 20.0], [480.0 - i as f64 * 15.0, 80.0] ], c.transform, g);
-            }*/
+             clear(background_color, g);
+             line(black,2f64 ,[100f64, 100f64, 200f64, 200f64], c.transform, g);
+
+
+             // attempt drawing rays around the cursor?
+             let rays = physics::generate_rays(11.0, mouse_pos);
+
+             // iterate over ray vec
+             for ray in rays {
+                 // create array for draw
+                 let draw_line = [
+                     ray.start_pos[0],
+                     ray.start_pos[1],
+                     ray.start_pos[0] + ray.vector[0] * 10_000f64,
+                     ray.start_pos[1] + ray.vector[1] * 10_000f64
+                 ];
+                 let color = ray.color;
+                 // draw ray
+                 line(color, 2.0, draw_line, c.transform, g);
+             }
+
+             // iterate over mirror vec
+             for mirror in mirrors.clone() {
+                 let draw_line = [
+                     mirror.start_pos[0],
+                     mirror.start_pos[1],
+                     mirror.end_pos[0],
+                     mirror.end_pos[1]
+                 ];
+                 let color = mirror.color;
+                 line(color, 3.0, draw_line, c.transform, g);
+             }
         });
     }
+
 }
 
+// use this function to simplify draw calls. maybe pass the draw args in and get em out?
+fn render() {
 
-fn generate_mirrors(path: &str, window_dimensions: [f64; 2]) -> Vec<Mirror> {
-    let mut mirrors: Vec<Mirror> = Vec::new();
+}
 
-    let json_data = io::read_json(path);
-    let coord_format = &json_data.coord_format;
-    let json_mirrors = &json_data.mirrors;
-    if coord_format == "pixels" {
-        mirrors = json_mirrors
-            .iter()
-            .map(|mirror| Mirror {
-                start_pos: physics::Vector2::new(mirror.start_pos[0], mirror.start_pos[1]),
-                end_pos: physics::Vector2::new(mirror.end_pos[0], mirror.end_pos[1]),
-                absorption_factor: mirror.absorption_factor as f32,
-            })
-            .collect();
-    } else if coord_format == "fractions" {
-        mirrors = json_mirrors
-            .iter()
-            .map(|mirror| Mirror {
-                start_pos: physics::Vector2::new(
-                    mirror.start_pos[0] * window_dimensions[0],
-                    mirror.start_pos[1] * window_dimensions[1],
-                ),
-                end_pos: physics::Vector2::new(
-                    mirror.end_pos[0] * window_dimensions[0],
-                    mirror.end_pos[1] * window_dimensions[1],
-                ),
-                absorption_factor: mirror.absorption_factor as f32,
-            })
-            .collect();
-    } else {
-        panic!("wrong coord type in the file at: {:}\nexiting...", path)
-    }
+fn handle_inputs() {
 
-    mirrors
 }
--- src/physics.rs
+++ src/physics.rs
@@ -1,262 +1,116 @@
-// structs for ray and mirror
+use piston_window::types::{Color, ColorComponent};
+use rand::prelude::*;
 
-#[derive(Debug, Copy, Clone)]
-pub(crate) struct Mirror {
-    pub(crate) start_pos: Vector2,
-    pub(crate) end_pos: Vector2,
-    pub(crate) absorption_factor: f32, // 0 = perfect mirror, 1 = nothing gets reflected
-}
-
-impl Mirror {
-    fn intersect(&self, ray: &Ray) -> IntersectionResult {
-        // skip intensity for now, needlessly complex
-        /* function description:
-        intersect self with ray using math. return the Result struct (cuz easier that way)
-
-         */
-
-        {
-            let a1: f64 = self.start_pos.x;
-            let a2: f64 = self.start_pos.y;
-            let b1: f64 = self.end_pos.x;
-            let b2: f64 = self.end_pos.y;
-            let p1: f64 = ray.start_pos.x;
-            let p2: f64 = ray.start_pos.y;
-            let v1: f64 = ray.vector.x;
-            let v2: f64 = ray.vector.y;
-
-            // result, will modify if successful
-            let mut result = IntersectionResult {
-                success: false,
-                position: Vector2::empty(),
-            };
-            // common denominator
-            let denominator = v2 * (b1 - a1) - v1 * (b2 - a2);
-            if denominator > 0.0 {
-                let m = ((b2 - a2) * (p1 - a1) - (b1 - a1) * (p2 - a2)) / denominator;
-                let n = (v2 * (p1 - a1) - v2 * (p2 - a2)) / denominator;
-                if 0.0 < n && 1.0 > n && m > 0.0 {
-                    result.position.x = a1 + n * (b1 - a1);
-                    result.position.y = a2 + n * (b2 - a2);
-                    result.success = true;
-
-                    // returns a successful collision
-                    result
-                } else {
-                    // result that returns false
-                    result
-                }
-            } else {
-                // result that returns false
-                result
-            }
-        }
-    }
-
-    fn normal_vector(&self) -> Vector2 {
-        // return the normal vector. we are assuming all mirrors are line mirrors, and that this is fine.
-        Vector2 {
-            x: self.end_pos.y - self.start_pos.x,
-            y: self.end_pos.x - self.start_pos.y,
-        }
-    }
-    pub(crate) fn clone(&self) -> Mirror {
-        Mirror {
-            start_pos: self.start_pos.clone(),
-            end_pos: self.end_pos.clone(),
-            absorption_factor: self.absorption_factor.clone(),
-        }
-    }
-}
-
-
-#[derive(Debug, Copy, Clone)]
-struct IntersectionResult {
+// structures to wrap return statements that need a success and data iff success
+struct IntersectResult {
     success: bool,
-    position: Vector2,
+    position: [f64;2]
 }
 
-#[derive(Debug, Copy, Clone)]
-struct ReflectionResult {
-    success: bool,
-    new_ray: Ray,
-    draw_ray: Ray,
-}
-impl ReflectionResult {
-    fn empty() -> ReflectionResult {
-        ReflectionResult {
+impl IntersectResult {
+    fn empty() -> IntersectResult {
+        IntersectResult {
             success: false,
-            new_ray: Ray::empty(),
-            draw_ray: Ray::empty(),
+            position: [f64::infinity();2]
         }
     }
 }
 
+// structures for Ray and Mirror
 #[derive(Debug, Copy, Clone)]
-pub(crate) struct Vector2 {
-    pub(crate) x: f64,
-    pub(crate) y: f64,
+pub struct Ray {
+    pub(crate) start_pos:[f64;2],
+    pub(crate) vector:[f64;2],
+    pub(crate) color:[ColorComponent;4],
 }
 
-
-impl Vector2 {
-    pub(crate) fn new(x: f64, y: f64) -> Self {
-        Self { x, y }
-    }
-    fn empty() -> Self {
-        Self {
-            x: f64::INFINITY,
-            y: f64::INFINITY,
-        }
-    }
-    fn dot(&self, vector: &Vector2) -> f64 {
-        let vector = vector.clone();
-        self.x * vector.x + self.y * vector.y
-    }
-
-    fn reflect(&self, normal: &Vector2) -> Vector2 {
-        let factor = 2.0 * self.dot(normal) / (normal.norm().powi(2));
-        let x: f64 = self.x - normal.x * factor;
-        let y: f64 = self.y - normal.y * factor;
-        Vector2::new(x, y)
-    }
-
-    fn norm(&self) -> f64 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
-    }
-
-    pub(crate) fn clone(&self) -> Vector2 {
-        Vector2::new(self.x.clone(), self.y.clone())
-    }
-    pub(crate) fn new_from_array(arr: [f64; 2]) -> Vector2 {
-        Vector2::new(arr[0], arr[1])
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-pub(crate) struct Ray {
-    pub(crate) start_pos: Vector2,
-    pub(crate) vector: Vector2,
-    pub(crate) intensity: f64, // below thresh: dead, above : alive (for example 0.1)
-}
 
 impl Ray {
-    fn new(start_pos: Vector2, vector: Vector2, intensity: f64) -> Ray {
+    // generate new ray object from input variables
+    fn new(start_pos: [f64;2], vector: [f64;2], color: [ColorComponent;4]) -> Ray {
         Ray {
             start_pos,
             vector,
-            intensity,
-        }
-    }
-
-    fn empty() -> Ray {
-        Ray {
-            start_pos: Vector2::empty(),
-            vector: Vector2::empty(),
-            intensity: 0.0,
-        }
-    }
-
-    pub(crate) fn distance(&self, position: Vector2) -> f64 {
-        ((self.start_pos.x - position.x).powi(2) + (self.start_pos.y - position.y).powi(2)).sqrt()
-    }
-
-    fn reflect(&self, position: Vector2, normal: Vector2) -> Ray {
-        // use vector math here (done)
-        Ray {
-            start_pos: position,
-            vector: self.vector.reflect(&normal),
-            intensity: 1.0,
-        }
-    }
-    pub(crate) fn clone(&self) -> Ray {
-        Ray {
-            start_pos: self.start_pos.clone(),
-            vector: self.vector.clone(),
-            intensity: self.intensity.clone(),
+            color,
         }
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+pub struct Mirror {
+    pub(crate) start_pos: [f64;2],
+    pub(crate) end_pos: [f64;2],
+    pub(crate) color: [ColorComponent;4]
+}
 
-fn find_closest_mirror(mut ray: Ray, mirrors: &Vec<Mirror>) -> ReflectionResult {
-    // initialize variable that tracks the distance to the closest mirror
-    let mut closest_distance = f64::MAX;
-    
-    let mut intersection_result = IntersectionResult {
-        success: false,
-        position: Vector2::empty(),
-    };
-    
-    let mut reflection_result = ReflectionResult::empty();
-    let mut normal = Vector2::empty();
-    let mut intensity = 1.0;
-    let mut closest_position = Vector2::empty();
+impl Mirror {
+    // intersection function for a ray as input
+    fn intersect() -> IntersectResult {
+        
 
-    for mirror in mirrors {
-        intersection_result = mirror.intersect(&ray);
-        if intersection_result.success {
-            let collision = intersection_result.position;
-            let distance = ray.distance(collision.clone());
-            if distance < closest_distance && closest_distance > (10f64.powi(-10)) {
-                closest_distance = distance;
-                closest_position = collision;
-                normal = mirror.normal_vector();
-                if mirror.absorption_factor == 1.0 {
-                    intensity = 0.0
-                } else {
-                    intensity = 1.0
-                };
+        IntersectResult::empty()
+    }
+}
+
+pub fn generate_mirrors(mirror_count: i32) -> Vec<Mirror> {
+    // for now the function spits out some pregenerated stuff
+    let min_rand = 10;
+    let max_rand = 700;
+
+    //
+    let black:[ColorComponent;4] = [0.0, 0.0, 0.0, 1.0];
+    let mut mirrors:Vec<Mirror> = Vec::new();
+
+    let mut rng = rand::rng();
+    for n in 0..mirror_count {
+        let start_pos = [
+            rng.random_range(min_rand..=max_rand) as f64,
+            rng.random_range(min_rand..=max_rand) as f64
+        ];
+        let end_pos = [
+            rng.random_range(min_rand..=max_rand) as f64,
+            rng.random_range(min_rand..=max_rand) as f64
+        ];
+
+        mirrors.push(
+            Mirror {
+                start_pos,
+                end_pos,
+                color:black
             }
-        }
+        )
     }
-    if closest_distance < f64::MAX {
-        // indicate success
-        reflection_result.success = true;
-        // set the new ray to be the important data
-        reflection_result.new_ray = ray.reflect(closest_position.clone(), normal);
-        reflection_result.new_ray.intensity = intensity;
-        reflection_result.draw_ray = Ray::new(
-            ray.start_pos,
-            closest_position,1.0
-        );
-        // println!("intersection succeeded with distance: {}\nOld Ray: {:?}\nNew Ray:{:?}\n\n", closest_distance, reflection_result.draw_ray, reflection_result.new_ray);
 
-    } else {
-        // println!("intersection failed")
-    }
-    // implicit return
-    reflection_result
+    mirrors
 }
 
-pub fn intersection_handler(
-    rays: Vec<Ray>,
-    mirrors: Vec<Mirror>,
-) -> [Vec<Ray>; 2] {
-    let mut new_rays: Vec<Ray> = vec![];
-    let mut draw_rays: Vec<Ray> = vec![];
+pub fn generate_rays(ray_count: f64, start: [f64;2]) -> Vec<Ray> {
+    // color of the ray. for now yellow
+    let yellow = [1.0, 1.0, 0.0, 1.0];
 
-    // there has to be a nicer way of iterating through rays, but eh for now
-    for ray in rays {
-        let reflection_result = find_closest_mirror(ray, &mirrors);
-        if reflection_result.success {
-            new_rays.push(reflection_result.draw_ray.clone());
-            
-            draw_rays.push(reflection_result.draw_ray.clone());
-        }
-    }
-    [new_rays, draw_rays]
-}
+    // vec that will contain the rays
+    let mut result: Vec<Ray> = Vec::new();
 
-pub fn generate_rays(start: Vector2, ray_count: i32) -> Vec<Ray> {
-    let _start = start.clone();
-    println!("{:?}", _start);
-    let mut new_rays: Vec<Ray> = vec![];
-    for n in 0..ray_count {
-        // angle should be between 1 and 2*pi radians
-        let angle: f64 = std::f64::consts::PI * 2.0 * (n as f64 / ray_count as f64);
-        let vector: Vector2 = Vector2::new(angle.cos(), angle.sin());
-        new_rays.push(Ray::new(_start, vector, 1.0));
+    // init pi, to make code more readable
+    let pi = std::f64::consts::PI;
+
+    // iter through the ray count to make rays
+    for mut n in 0..ray_count as i32 {
+        // angle: use fraction of 2pi
+        let m = n as f64;
+        let angle = 2f64 * pi * m / ray_count;
+        // vector is simplified trigonometry
+        let vector:[f64;2] = [angle.cos(), angle.sin()];
+        // create ray obj
+        let ray = Ray {
+            start_pos: start,
+            vector,
+            color: yellow
+        };
+        // add ray to vec
+        result.push(ray);
     }
-    new_rays
+
+    // return final vec
+    result
 }
